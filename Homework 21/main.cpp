14db5bb70f64f11e58998548b96d5a13be644848
Aleksey <Aleksey@DESKTOP-J6BL2Q2>
2023-09-27 13:12:13 +0500

    Добавьте файлы проекта.

---
+++ Homework 21/main.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+#include<ctime>
+#include<cstdlib>
+
+int main()
+{
+	std::cout << "Enter two nimbers - ";
+	int X, Y;
+	std::cin >> X >> Y;
+
+	int* px = &X, *py = &Y;
+
+	std::cout << "X= " << X << " px = " << *px << " Y= " << Y << " py = " << *py << "\n";
+	
+	int temp = *px;
+	*px = *py;
+	*py = temp;
+
+	std::cout << "X= " << X << " px = " << *px << " Y= " << Y << " py = " << *py << "\n";
+
+
+
+	return 0;
+}
